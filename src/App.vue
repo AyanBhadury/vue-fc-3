4432660711afa7c1b95cd5d71355222bb962f00a
AyanBhadury <ayon.bony@gmail.com>
2021-09-08 13:23:03 +0530

    charts added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Charts />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Charts from "./components/Charts";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Charts,
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueFusionCharts from 'vue-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import Column2D from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-createApp(App).mount('#app')
+//import Charts from './components/Charts'
+
+const app = createApp(App);
+//app.component('Charts', Charts);
+app.use(VueFusionCharts, FusionCharts, Column2D, FusionTheme);
+app.mount('#app')
+
+
